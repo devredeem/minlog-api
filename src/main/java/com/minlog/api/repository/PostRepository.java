318dc62d0937b6804cddc3cb464a5a182239cbf9
devredeem <morangie33@gmail.com>
2022-07-03 14:55:13 +0900

    first commit

---
+++ src/main/java/com/minlog/api/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.minlog.api.repository;
+
+import com.minlog.api.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
